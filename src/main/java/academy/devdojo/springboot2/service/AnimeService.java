61af9ec1c55cafffa708be4119439f64eb637ee3
hederdavid <hedermd6@gmail.com>
2024-08-19 09:53:34 -0300

    feat: CRUD

---
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -0,0 +1,45 @@
+package academy.devdojo.springboot2.service;
+
+import academy.devdojo.springboot2.domain.Anime;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Service
+public class AnimeService {
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>();
+        animes.add(new Anime(1L, "One Piece"));
+        animes.add(new Anime(2L, "DBZ"));
+    }
+    public List<Anime> listAll() {
+        return animes;
+    }
+
+    public Anime findById(Long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 1000000));
+        animes.add(anime);
+        return anime;
+    }
+
+    public void delete(long id) {
+        animes.remove(findById(id));
+    }
+
+    public void replace(Anime anime) {
+        delete(anime.getId());
+        save(anime);
+    }
+}
